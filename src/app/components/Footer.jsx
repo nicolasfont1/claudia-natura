0dd4cfc352426268b20f2ae8127a3c91f526cd86
nicolasfont1 <nicolasfont15@gmail.com>
2024-01-25 15:23:52 -0300

    footer component

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,20 @@
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+const Footer = () => {
+	return (
+		<Stack justifyContent="center" alignItems="center" sx={{ bgcolor: "#dde8ee", width: "100%", height: "50px" }}>
+			<Typography
+				fontWeight="sm"
+				level="body-sm"
+        textColor="neutral.500"
+        sx={{ opacity: 0.6 }}
+				endDecorator={<FavoriteIcon fontSize="sm" />}>
+				Hecho con amor para mamá
+			</Typography>
+		</Stack>
+	);
+};
+
+export default Footer;
