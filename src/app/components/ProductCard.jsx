59f9e155caf64e7c84123679b87268abcefb01cd
nicolasfont1 <nicolasfont15@gmail.com>
2024-01-26 16:07:47 -0300

    added functionality to product card

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -8,57 +8,47 @@ import CardContent from "@mui/joy/CardContent";
 import AspectRatio from "@mui/joy/AspectRatio";
 import CardOverflow from "@mui/joy/CardOverflow";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ByOrderText from "./ByOrderText";
+import InmediateDeliveryText from "./InmediateDeliveryText";
 
-const ProductCard = ({}) => {
+const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, image }) => {
 	return (
 		<Card orientation="horizontal" variant="soft" sx={{ width: "90%", bgcolor: "#efebe9", maxWidth: 540 }}>
 			<CardOverflow sx={{ bgcolor: "#fff", alignContent: "center" }}>
 				<AspectRatio ratio="1" sx={{ width: 140 }}>
-					<img
-						src="https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dw5d10df90/NATARG-74900_1.jpg"
-						loading="lazy"
-						alt=""
-					/>
+					<img src={image} loading="lazy" alt="" />
 				</AspectRatio>
 			</CardOverflow>
 			<CardContent>
 				{/* PRODUCT NAME AND SIZE */}
 				<Stack direction="row" justifyContent="space-between" sx={{ mb: 0.5 }}>
 					<Typography fontWeight="lg" level="body-sm" textColor="success.plainColor" lineHeight={1.1}>
-						Crema nutritiva
+						{name}
 					</Typography>
 					<Typography fontWeight="xs" level="body-xs" textColor="#9e9e9e" sx={{ ml: 1 }}>
-						400ml
+						{size}ml
 					</Typography>
 				</Stack>
 
 				{/* SEE FRAGANCES AND DELIVERY AVAILABILITY */}
 				<Stack direction="column">
-					<Link href={`/${'crema-nutritiva'}`} level="body-xs" sx={{ fontWeight: 400, opacity: 0.7 }}>
+					<Link href={`/${"crema-nutritiva"}`} level="body-xs" sx={{ fontWeight: 400, opacity: 0.7 }}>
 						Ver fragancias disponibles
 					</Link>
-					<Typography
-						fontWeight="xs"
-						level="body-xs"
-						textColor="neutral"
-						sx={{ opacity: 0.6 }}
-						startDecorator={<CheckCircleOutlineIcon sx={{ color: "neutral", fontSize: 15 }} />}>
-						Entrega inmediata
-					</Typography>
+					{inmediateDelivery ? <InmediateDeliveryText /> : <ByOrderText />}
 				</Stack>
 
 				{/* ORIGINAL PRICE AND CLAUDIA PRICE */}
 				<Stack direction="column">
 					<Typography fontWeight="xs" level="body-xs" color="danger" sx={{ opacity: 0.8 }}>
-						Precio de revista: $8900
+						Precio de revista: ${magazinePrice}
 					</Typography>
 					<Stack direction="row" justifyContent="space-between">
 						<Typography fontWeight="md" level="body-md" textColor="success.plainColor">
-							$7000
+							${price}
 						</Typography>
 						<IconButton color="success" size="xs">
-							<Link href={`/${'crema-nutritiva'}`} color="success">
+							<Link href={`/${id}`} color="success">
 								{<AddCircleOutlineIcon />}
 							</Link>
 						</IconButton>
