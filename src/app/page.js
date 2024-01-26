626116ba5c954d8af4fb1fe8f55dbc931732e416
nicolasfont1 <nicolasfont15@gmail.com>
2024-01-26 16:41:34 -0300

    changed tab format

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,7 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import Navbar from './components/Navbar';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Stack from "@mui/joy/Stack";
-import SearchIcon from '@mui/icons-material/Search';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import Typography from "@mui/joy/Typography";
 import Box from "@mui/system/Box";
 
 export default function Home() {
@@ -23,28 +22,26 @@ export default function Home() {
           <Tabs aria-label="Application tabs" defaultValue={0}>
             <TabList tabFlex={1}>
               <Tab orientation='vertical'>
-                <ListItemDecorator>
-                  <FormatListBulletedIcon sx={{ color: "neutral" }} />
-                </ListItemDecorator>
+                <Typography fontWeight="md"
+                  level="body-md"
+                  textColor="neutral"
+                  endDecorator={<FormatListBulletedIcon sx={{ color: "neutral", fontSize: 18 }} />}>
+                  Productos
+                </Typography>
               </Tab>
               <Tab orientation='vertical'>
-                <ListItemDecorator>
-                  <SearchIcon sx={{ color: "neutral" }} />
-                </ListItemDecorator>
-              </Tab>
-              <Tab orientation='vertical'>
-                <ListItemDecorator>
-                  <ShoppingCartIcon sx={{ color: "neutral" }} />
-                </ListItemDecorator>
+                <Typography fontWeight="md"
+                  level="body-md"
+                  textColor="neutral"
+                  endDecorator={<ShoppingCartIcon sx={{ color: "neutral", fontSize: 18 }} />}>
+                  Mi pedido
+                </Typography>
               </Tab>
             </TabList>
             <TabPanel value={0} sx={{ bgcolor: '#bcaaa4' }}>
               <ProductList />
             </TabPanel>
             <TabPanel value={1} sx={{ bgcolor: '#bcaaa4' }}>
-              <b>Búsqueda</b>
-            </TabPanel>
-            <TabPanel value={2} sx={{ bgcolor: '#bcaaa4' }}>
               <b>Carrito de compras</b>
             </TabPanel>
           </Tabs>
