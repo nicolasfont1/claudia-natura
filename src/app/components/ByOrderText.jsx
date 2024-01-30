f723c7d7248c7ef0338b8b27c059f593fc299a6f
nicolasfont1 <nicolasfont15@gmail.com>
2024-01-30 16:52:30 -0300

    updated component for reusability

--- src/app/components/ByOrderText.jsx
+++ src/app/components/ByOrderText.jsx
@@ -1,14 +1,19 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Typography from "@mui/joy/Typography";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 const ByOrderText = () => {
+	const path = usePathname();
+	let isProductList = path === "/"
+
 	return (
 		<Typography
-			fontWeight="xs"
-			level="body-xs"
+			fontWeight={isProductList ? "xs" : "md"}
+			level={isProductList ? "body-xs" : "h4"}
 			textColor="neutral"
 			sx={{ opacity: 0.6 }}
-			startDecorator={<AccessTimeIcon sx={{ color: "neutral", fontSize: 15 }} />}>
+			startDecorator={<AccessTimeIcon sx={{ color: "neutral", fontSize: `${isProductList ? "15" : "18"}` }} />}>
 			Por encargo
 		</Typography>
 	);
