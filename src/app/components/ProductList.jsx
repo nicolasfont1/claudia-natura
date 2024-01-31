4c76d5584f568f5fb9fd005c158d43c6ca01e653
nicolasfont1 <nicolasfont15@gmail.com>
2024-01-31 18:04:57 -0300

    new product list logic

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -2,26 +2,40 @@
 import Stack from "@mui/joy/Stack";
 import ProductCard from "./ProductCard";
 import ProductsJSON from "../../resources/product-list.json";
+import { useState } from "react";
+import SearchHub from "./SearchHub";
+import Box from "@mui/system/Box";
 
 const ProductList = () => {
+	const [filteredProducts, setFilteredProducts] = useState([]);
+	const [productName, setProductName] = useState("");
+
+	const handleSearchbar = (event) => {
+		setProductName(event.target.value);
+		console.log(productName)
+	};
+
 	return (
-		<Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
-			{ProductsJSON.map((product, index) => {
-				return (
-					<ProductCard
-						key={index}
-						id={product.id}
-						name={product.name}
-						size={product.size}
-						sizeUnit={product.sizeUnit}
-						inmediateDelivery={product.inmediateDelivery}
-						magazinePrice={product.magazinePrice}
-						price={product.price}
-						image={product.image}
-					/>
-				);
-			})}
-		</Stack>
+		<Box>
+			<SearchHub handleSearchbar={ handleSearchbar } />
+			<Stack direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{ px: 1 }}>
+				{/* {ProductsJSON.map((product, index) => {
+					return (
+						<ProductCard
+							key={index}
+							id={product.id}
+							name={product.name}
+							size={product.size}
+							sizeUnit={product.sizeUnit}
+							inmediateDelivery={product.inmediateDelivery}
+							magazinePrice={product.magazinePrice}
+							price={product.price}
+							image={product.image}
+						/>
+					);
+				})} */}
+			</Stack>
+		</Box>
 	);
 };
 export default ProductList;
