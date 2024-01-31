2a322be3e0fdac85d661e9c1aaf9b7b5b5c898e6
nicolasfont1 <nicolasfont15@gmail.com>
2024-01-31 18:04:29 -0300

    added margin to text

--- src/app/components/InmediateDeliveryText.jsx
+++ src/app/components/InmediateDeliveryText.jsx
@@ -1,18 +1,23 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Typography from "@mui/joy/Typography";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import CheckIcon from "@mui/icons-material/Check";
 
 const InmediateDeliveryText = () => {
 	const path = usePathname();
-	let isProductList = path === "/"
+	let isProductList = path === "/";
 
 	return (
 		<Typography
 			fontWeight={isProductList ? "xs" : "md"}
 			level={isProductList ? "body-xs" : "title-sm"}
-			textColor={isProductList ? "#909497" : "neutral.200" }
-			startDecorator={<CheckCircleOutlineIcon sx={{ fontSize: `${isProductList ? "15" : "18"}`, color: `${isProductList ? "#909497" : "neutral.200"}` }} />}>
+			textColor={isProductList ? "#909497" : "neutral.200"}
+			sx={{ mr: 1 }}
+			startDecorator={
+				<CheckIcon
+					sx={{ fontSize: `${isProductList ? "15" : "18"}`, color: `${isProductList ? "#909497" : "neutral.200"}` }}
+				/>
+			}>
 			Entrega inmediata
 		</Typography>
 	);
