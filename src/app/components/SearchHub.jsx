a1924b9191897bac51138631752348725f57f7d8
nicolasfont1 <nicolasfont15@gmail.com>
2024-02-02 17:18:43 -0300

    applied infinite scroll functions

--- src/app/components/SearchHub.jsx
+++ src/app/components/SearchHub.jsx
@@ -1,5 +1,4 @@
 import Stack from "@mui/joy/Stack";
-import Typography from "@mui/joy/Typography";
 import Select from "@mui/joy/Select";
 import Option from "@mui/joy/Option";
 import Input from "@mui/joy/Input";
@@ -7,24 +6,68 @@ import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/joy/IconButton";
 import CloseRounded from "@mui/icons-material/CloseRounded";
 
-const SearchHub = ({ handleSearchbar }) => {
+const SearchHub = ({ action, productCathegory, setProductCathegory, setProductName, productName, handleKeyUp }) => {
 	return (
 		<Stack sx={{ m: 1 }} direction="row" justifyContent="space-between">
 			<Input
+				value={productName}
 				variant="soft"
 				size="sm"
 				placeholder="Búsqueda..."
 				endDecorator={
-					<IconButton size="sm">
-						<SearchIcon />
-					</IconButton>
+					productName ? (
+						<IconButton
+							sx={{ maxHeight: 32 }}
+							size="sm"
+							variant="plain"
+							color="neutral"
+							onMouseDown={(event) => {
+								event.stopPropagation();
+							}}
+							onClick={() => {
+								setProductName("");
+							}}>
+							<CloseRounded />
+						</IconButton>
+					) : (
+						<IconButton size="sm">
+							<SearchIcon />
+						</IconButton>
+					)
 				}
-				sx={{ width: "65%", "--Input-focusedThickness": "0.1rem" }}
-				onChange={handleSearchbar}
+				sx={{ width: "60%", "--Input-focusedThickness": "0rem", fontSize: 16 }}
+				onChange={(event) => setProductName(event.target.value)}
+				onKeyUp={handleKeyUp}
 			/>
-			<Select variant="soft" size="sm" placeholder="Filtrar" sx={{ width: "25%" }}>
-				<Option value={"perfumeria"}>Perfumería</Option>
-				<Option value={"belleza"}>Belleza</Option>
+			<Select
+				variant="soft"
+				size="sm"
+				placeholder="Filtrar"
+				sx={{ width: "33%", maxHeight: 32, fontSize: 15 }}
+				onChange={(event, newValue) => setProductCathegory(newValue)}
+				value={productCathegory}
+				action={action}
+				{...(productCathegory && {
+					endDecorator: (
+						<IconButton
+							sx={{ maxHeight: 32 }}
+							size="sm"
+							variant="plain"
+							color="neutral"
+							onMouseDown={(event) => {
+								event.stopPropagation();
+							}}
+							onClick={() => {
+								setProductCathegory(null);
+								action.current?.focusVisible();
+							}}>
+							<CloseRounded />
+						</IconButton>
+					),
+					indicator: null,
+				})}>
+				<Option value="perfumeria">Perfumería</Option>
+				<Option value="belleza">Belleza</Option>
 			</Select>
 		</Stack>
 	);
