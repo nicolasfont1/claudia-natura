08a2d8e376d4b79788c20c59fff07627705a306c
nicolasfont1 <nicolasfont15@gmail.com>
2024-02-07 10:52:16 -0300

    implemented store provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "@/store/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="theme-color" content="#dde8ee"/>
-      <body className={inter.className} style={{margin: 0}}>{children}</body>
+      <meta name="theme-color" content="#dde8ee" />
+      <Providers>
+        <body className={inter.className} style={{ margin: 0 }}>{children}</body>
+      </Providers>
     </html>
   );
 }
