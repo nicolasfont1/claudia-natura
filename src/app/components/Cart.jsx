6d5476aabbf340e4e8e09f3f3fc8e9c1597c1df6
nicolasfont1 <nicolasfont15@gmail.com>
2024-02-07 17:55:18 -0300

    cart update

--- src/app/components/Cart.jsx
+++ src/app/components/Cart.jsx
@@ -1,16 +1,15 @@
 "use client";
+import Typography from "@mui/joy/Typography";
 import Box from "@mui/system/Box";
+import Stack from "@mui/joy/Stack";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import EmptyCartText from "./EmptyCartText";
 
 import { useSelector } from "react-redux";
 
 const Cart = () => {
-  const cartState = useSelector(state => state.cart.cart) 
+	const cartState = useSelector((state) => state.cart.cart);
 
-  return (
-    <Box>
-      <b>Acá esta el carro</b>
-      <p>{cartState}</p>
-    </Box>
-  )
+	return <Box>{!cartState.length ? <EmptyCartText /> : <Typography></Typography>}</Box>;
 };
 export default Cart;
