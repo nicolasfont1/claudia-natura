048e12928caa0e86d3adc18ecdd699b7ee9ec367
nicolasfont1 <nicolasfont15@gmail.com>
2024-02-11 14:55:31 -0300

    removed testing products from cart

--- src/store/slice.js
+++ src/store/slice.js
@@ -3,35 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 export const Slice = createSlice({
   name: 'cart',
   initialState: {
-    cart: [
-      {
-        name: 'Crema nutritiva',
-        image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dw5d10df90/NATARG-74900_1.jpg',
-        variant: 'frutas rojas',
-        amount: 2,
-        price: 7000
-      },
-      {
-        name: 'Kaiak aero eau de toilette femenino',
-        image: 'https://tuseduccion.com.ar/14637-home_default/kaiak-aero-femenino-edt-100ml-natura.jpg',
-        variant: 'cítrico floral',
-        amount: 1,
-        price: 0
-      },
-      {
-        name: 'Kaiak aventura eau de toilette femenino',
-        image: 'https://fraguru.com/mdimg/perfume/375x500.43623.jpg',
-        variant: 'floral acuático',
-        amount: 1,
-        price: 0
-      }
-    ]
+    cart: []
   },
   reducers: {
     addItem: (state, action) => {
       state.cart = [...state.cart, action.payload]
+    },
+    deleteItem: (state, action) => {
+      state.cart = state.cart.filter((product) => {
+        return (product.name !== action.payload.name && product.variant !== action.payload.variant)
+      })
     }
+    //revisar que cuando pongo productos iguales se borran todos
   }
 })
 
-export const { addItem } = Slice.actions;
+export const { addItem, deleteItem } = Slice.actions;
