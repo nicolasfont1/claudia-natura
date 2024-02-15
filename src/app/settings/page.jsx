c9c6b1c51024415267106660ec34ee3659df9af9
nicolasfont1 <nicolasfont15@gmail.com>
2024-02-15 11:17:42 -0300

    fixed width on lg screen devices

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,33 +1,36 @@
 import Box from "@mui/system/Box";
 import Typography from "@mui/joy/Typography";
 import Navbar from "../components/Navbar";
+import Stack from "@mui/joy/Stack";
 
 const SettingsPage = () => {
 	return (
-		<Box
-			sx={{
-				minHeight: "100svh",
-			}}>
-			<Navbar backPath={'/'} />
+		<Stack alignItems="center">
 			<Box
 				sx={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-          justifyContent: "center",
-          height: '90svh'
+					minHeight: "100svh", width: '100%', maxWidth: 540, position: 'relative', borderLeft: 1, borderRight: 1, borderColor: '#dde8ee'
 				}}>
-				<Typography fontWeight="md" level="body-lg" textColor="neutral">
-					Hola persona curiosa,
-				</Typography>
-				<Typography fontWeight="md" level="body-lg" textColor="neutral">
-          esta sección sigue en desarrollo.
-        </Typography>
-        <Typography fontWeight="md" level="body-lg" textColor="neutral">
-          {`:)`}
-				</Typography>
+				<Navbar backPath={'/'} />
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+								justifyContent: "center",
+								height: '90svh'
+					}}>
+					<Typography fontWeight="md" level="body-lg" textColor="neutral">
+						Hola persona curiosa,
+					</Typography>
+					<Typography fontWeight="md" level="body-lg" textColor="neutral">
+								esta sección sigue en desarrollo.
+							</Typography>
+							<Typography fontWeight="md" level="body-lg" textColor="neutral">
+								{`:)`}
+					</Typography>
+				</Box>
 			</Box>
-		</Box>
+		</Stack>
 	);
 };
 export default SettingsPage;
