0c81bfb97eef12fd5cfdae3d037fab1c9b502b9c
nicolasfont1 <nicolasfont15@gmail.com>
2024-03-13 13:16:18 -0300

    added repuestos to card

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -11,10 +11,10 @@ import InmediateDeliveryText from "./InmediateDeliveryText";
 
 const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, image, sizeUnit }) => {
 	return (
-		<Link href={`/${id}`} style={{width: '96%', maxWidth: 540, textDecoration: 'none'}}>
-			<Card orientation="horizontal" variant="soft" sx={{ bgcolor: "#efebe9" }} >
+		<Link href={`/${id}`} style={{ width: "96%", maxWidth: 540, textDecoration: "none" }}>
+			<Card orientation="horizontal" variant="soft" sx={{ bgcolor: "#efebe9" }}>
 				<CardOverflow sx={{ bgcolor: "#fff", alignContent: "center" }}>
-					<AspectRatio ratio="1" sx={{ width: 140, alignSelf: 'center' }} objectFit="contain">
+					<AspectRatio ratio="1" sx={{ width: 140, alignSelf: "center" }} objectFit="contain">
 						<img src={image} loading="lazy" alt={name} style={{ background: "#FFFFFF" }} />
 					</AspectRatio>
 				</CardOverflow>
@@ -24,30 +24,22 @@ const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, 
 							{name}
 						</Typography>
 						<Typography fontWeight="xs" level="body-xs" textColor="#9e9e9e" sx={{ ml: 1 }}>
-							{size}{sizeUnit}
+							{size}
+							{sizeUnit}
 						</Typography>
 					</Stack>
 					<Stack direction="column">
-						{/* <Link href={`/${id}`} style={{textDecoration: 'none'}}>
-							<Typography level="body-xs" sx={{ fontWeight: 400, opacity: 0.7 }} color="primary">Ver fragancias disponibles</Typography>
-						</Link> */}
+						<Typography level="body-xs" sx={{ fontWeight: 400, opacity: 0.7 }} color="primary">
+							Repuestos disponibles
+						</Typography>
 						{inmediateDelivery ? <InmediateDeliveryText /> : <ByOrderText />}
-					</Stack>
-					<Stack direction="column">
 						<Typography fontWeight="xs" level="body-xs" color="danger" sx={{ opacity: 0.8 }}>
 							Precio de revista: ${magazinePrice}
 						</Typography>
-						<Stack direction="row" justifyContent="space-between">
-							<Typography fontWeight="md" level="body-md" textColor="success.plainColor">
-								${price}
-							</Typography>
-							{/* <IconButton color="success" size="xs">
-								<Link href={`/${id}`}>
-									{<AddCircleOutlineIcon color="success" />}
-								</Link>
-							</IconButton> */}
-						</Stack>
 					</Stack>
+					<Typography fontWeight="md" level="body-md" textColor="success.plainColor">
+						${price}
+					</Typography>
 				</CardContent>
 			</Card>
 		</Link>
