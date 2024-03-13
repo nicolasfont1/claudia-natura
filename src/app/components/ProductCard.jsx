17a3d23bf49b2db053aecffb9b8f0816efe88737
nicolasfont1 <nicolasfont15@gmail.com>
2024-03-13 15:08:02 -0300

    added refillment to card

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -9,7 +9,7 @@ import CardOverflow from "@mui/joy/CardOverflow";
 import ByOrderText from "./ByOrderText";
 import InmediateDeliveryText from "./InmediateDeliveryText";
 
-const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, image, sizeUnit }) => {
+const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, image, sizeUnit, refillment }) => {
 	return (
 		<Link href={`/${id}`} style={{ width: "96%", maxWidth: 540, textDecoration: "none" }}>
 			<Card orientation="horizontal" variant="soft" sx={{ bgcolor: "#efebe9" }}>
@@ -20,7 +20,7 @@ const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, 
 				</CardOverflow>
 				<CardContent>
 					<Stack direction="row" justifyContent="space-between" sx={{ mb: 0.5 }}>
-						<Typography fontWeight="lg" level="body-sm" textColor="success.plainColor" lineHeight={1.1}>
+						<Typography fontWeight="lg" level="body-sm" textColor="success.plainColor" lineHeight={1.1} sx={refillment ? {} : {mb: 1}}>
 							{name}
 						</Typography>
 						<Typography fontWeight="xs" level="body-xs" textColor="#9e9e9e" sx={{ ml: 1 }}>
@@ -29,9 +29,11 @@ const ProductCard = ({ id, name, size, inmediateDelivery, magazinePrice, price, 
 						</Typography>
 					</Stack>
 					<Stack direction="column">
-						<Typography level="body-xs" sx={{ fontWeight: 400, opacity: 0.7 }} color="primary">
-							Repuestos disponibles
-						</Typography>
+						{refillment && (
+							<Typography level="body-xs" sx={{ opacity: 0.7 }} color="">
+								Repuestos disponibles
+							</Typography>
+						)}
 						{inmediateDelivery ? <InmediateDeliveryText /> : <ByOrderText />}
 						<Typography fontWeight="xs" level="body-xs" color="danger" sx={{ opacity: 0.8 }}>
 							Precio de revista: ${magazinePrice}
