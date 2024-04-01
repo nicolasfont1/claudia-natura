1a2182ef4677eb5be9bd8b89045083958e094263
nicolasfont1 <nicolasfont15@gmail.com>
2024-04-01 12:54:46 -0300

    added product id for cart

--- src/app/components/CartItemCard.jsx
+++ src/app/components/CartItemCard.jsx
@@ -14,57 +14,63 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { useState } from "react";
 import DeleteItemModal from "./DeleteItemModal";
 
-const CartItemCard = ({ name, image, variant, amount, price }) => {
+const CartItemCard = ({ name, image, variant, amount, price, id }) => {
 	const [modalOpen, setModalOpen] = useState(false);
 
 	return (
 		<Card orientation="horizontal" variant="outlined" sx={{ width: "95%", bgcolor: "#efebe9", maxWidth: 540, p: 0.8 }}>
-			<DeleteItemModal setModalOpen={setModalOpen} modalOpen={modalOpen} itemName={name} itemVariant={variant} />
+			<DeleteItemModal
+				setModalOpen={setModalOpen}
+				modalOpen={modalOpen}
+				itemName={name}
+				itemVariant={variant}
+				itemId={id}
+			/>
 			<CardOverflow sx={{ background: "#FFFFFF" }}>
 				<AspectRatio ratio="1" sx={{ width: 90, alignSelf: "center" }} objectFit="contain">
 					<img src={image} loading="lazy" style={{ background: "#FFFFFF" }} />
 				</AspectRatio>
 			</CardOverflow>
 			<CardContent>
-				<Stack direction="column" justifyContent="space-between" sx={{height: '100%'}}>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography fontWeight="md" level="body-sm" textColor="neutral.600" lineHeight={1.1}>
-              {name}
-            </Typography>
-            <Dropdown>
-              <MenuButton
-                sx={{ height: 15 }}
-                slots={{ root: IconButton }}
-                slotProps={{ root: { variant: "plain", color: "neutral", size: "xs" } }}>
-                <MoreHorizIcon />
-              </MenuButton>
-              <Menu placement="bottom-end" variant="soft" sx={{ p: 0 }}>
-                {/* <MenuItem>Editar</MenuItem> */}
-                <MenuItem onClick={() => setModalOpen(true)}>
-                  <DeleteIcon />
-                  <Typography level="body-sm">Eliminar</Typography>
-                </MenuItem>
-              </Menu>
-            </Dropdown>
-          </Stack>
-          <Typography level="body-xs" textColor="neutral.400">
-            {variant}
-          </Typography>
-          {amount !== 0 && (
-            <Typography level="body-xs" textColor="neutral.400">
-              {amount} {amount > 1 ? "unidades" : "unidad"}
-            </Typography>
-          )}
-          {price === 0 ? (
-            <Typography level="body-md" fontWeight="md" color="success">
-              Consulta de precio
-            </Typography>
-          ) : (
-            <Typography level="body-md" fontWeight="md" color="success">
-              ${price * amount}
-            </Typography>
-          )}
-        </Stack>
+				<Stack direction="column" justifyContent="space-between" sx={{ height: "100%" }}>
+					<Stack direction="row" justifyContent="space-between">
+						<Typography fontWeight="md" level="body-sm" textColor="neutral.600" lineHeight={1.1}>
+							{name}
+						</Typography>
+						<Dropdown>
+							<MenuButton
+								sx={{ height: 15 }}
+								slots={{ root: IconButton }}
+								slotProps={{ root: { variant: "plain", color: "neutral", size: "xs" } }}>
+								<MoreHorizIcon />
+							</MenuButton>
+							<Menu placement="bottom-end" variant="soft" sx={{ p: 0 }}>
+								{/* <MenuItem>Editar</MenuItem> */}
+								<MenuItem onClick={() => setModalOpen(true)}>
+									<DeleteIcon />
+									<Typography level="body-sm">Eliminar</Typography>
+								</MenuItem>
+							</Menu>
+						</Dropdown>
+					</Stack>
+					<Typography level="body-xs" textColor="neutral.400">
+						{variant}
+					</Typography>
+					{amount !== 0 && (
+						<Typography level="body-xs" textColor="neutral.400">
+							{amount} {amount > 1 ? "unidades" : "unidad"}
+						</Typography>
+					)}
+					{price === 0 ? (
+						<Typography level="body-md" fontWeight="md" color="success">
+							Consulta de precio
+						</Typography>
+					) : (
+						<Typography level="body-md" fontWeight="md" color="success">
+							${price * amount}
+						</Typography>
+					)}
+				</Stack>
 			</CardContent>
 		</Card>
 	);
